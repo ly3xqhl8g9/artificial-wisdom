ac36619e6e471ce1fec63da6e0f25cd7216f22e7
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-27 22:37:23 +0200

    setup: package

---
+++ tests/test_ponderer.py
@@ -0,0 +1,16 @@
+import unittest
+import pprint
+
+from artificial_wisdom.main import Ponderer
+
+
+
+class TestPonderer(unittest.TestCase):
+   def test_ponderer(self):
+        ponderer = Ponderer()
+
+        assert 5 + 7 == 12
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_wonderer.py
@@ -0,0 +1,16 @@
+import unittest
+import pprint
+
+from artificial_wisdom.main import Wonderer
+
+
+
+class TestWonderer(unittest.TestCase):
+   def test_wonderer(self):
+        wonderer = Wonderer()
+
+        assert 5 + 7 == 12
+
+
+if __name__ == '__main__':
+    unittest.main()
